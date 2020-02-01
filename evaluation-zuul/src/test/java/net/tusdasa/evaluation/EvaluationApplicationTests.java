38b1341c3f10a6db4e1141d0b838a9d4f761e465
tusdasa <none@none>
2020-02-01 20:19:27 +0800

    整理代码，使用ci构建
    --HG--
    branch : develement

--- evaluation-zuul/src/test/java/net/tusdasa/evaluation/EvaluationApplicationTests.java
+++ evaluation-zuul/src/test/java/net/tusdasa/evaluation/EvaluationApplicationTests.java
@@ -3,7 +3,7 @@ package net.tusdasa.evaluation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class EvaluationApplicationTests {
 
 	@Test
