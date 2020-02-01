4a8ccaedf19113ad06ae36529f6828108d2c7c09
tusdasa <none@none>
2020-02-01 17:03:00 +0800

    增加打包插件，完成第三指标微服务

--- evaluation-service/thirdkpi-service/src/main/java/net/tusdasa/evaluation/controller/ThirdKpiController.java
+++ evaluation-service/thirdkpi-service/src/main/java/net/tusdasa/evaluation/controller/ThirdKpiController.java
@@ -3,9 +3,8 @@ package net.tusdasa.evaluation.controller;
 import net.tusdasa.evaluation.commons.CommonResponse;
 import net.tusdasa.evaluation.entity.ThirdKpi;
 import net.tusdasa.evaluation.service.ThirdKpiService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import net.tusdasa.evaluation.vo.ThirdKpiRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ThirdKpiController {
@@ -17,6 +16,11 @@ public class ThirdKpiController {
 
     }
 
+    @GetMapping("/")
+    public CommonResponse<ThirdKpi> findAll() {
+        return new CommonResponse<ThirdKpi>().ok().table(this.thirdKpiService.findAll());
+    }
+
     @GetMapping("/{id}")
     public CommonResponse<ThirdKpi> findById(@PathVariable("id") Integer id) {
         ThirdKpi thirdKpi = this.thirdKpiService.findThirdKpiById(id);
@@ -25,6 +29,29 @@ public class ThirdKpiController {
             return new CommonResponse<ThirdKpi>().ok().data(thirdKpi);
         }
         return new CommonResponse<ThirdKpi>().error("未找到");
+    }
+
+    @GetMapping("/second/{id}")
+    public CommonResponse<ThirdKpi> findSecondKpiId(@PathVariable("id") Integer secondKpiId) {
+        return new CommonResponse<ThirdKpi>().ok().table(this.thirdKpiService.findBySecondKpiId(secondKpiId));
+    }
+
+    @PostMapping("/")
+    public CommonResponse<String> createThirdKpi(@RequestBody ThirdKpiRequest request) {
+        if (request.isCreateRequest()) {
+            this.thirdKpiService.addThirdKpi(request);
+            return new CommonResponse<String>().ok();
+        }
+        return new CommonResponse<String>().error();
+    }
+
+    @PutMapping("/")
+    public CommonResponse<String> updateThirdKpi(@RequestBody ThirdKpiRequest request) {
+        if (request.isUpdateRequest()) {
+            this.thirdKpiService.updateThirdKpi(request);
+            return new CommonResponse<String>().ok();
+        }
+        return new CommonResponse<String>().error();
     }
 
 
