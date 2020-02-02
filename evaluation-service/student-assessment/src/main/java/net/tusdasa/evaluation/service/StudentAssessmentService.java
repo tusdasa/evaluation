b0aed5f303f53c3ed0b8d23b32c1e239915f7750
tusdasa <none@none>
2020-02-02 12:49:23 +0800

    添加接口方法
    --HG--
    branch : develement

--- evaluation-service/student-assessment/src/main/java/net/tusdasa/evaluation/service/StudentAssessmentService.java
+++ evaluation-service/student-assessment/src/main/java/net/tusdasa/evaluation/service/StudentAssessmentService.java
@@ -1,4 +1,12 @@
 package net.tusdasa.evaluation.service;
 
+import net.tusdasa.evaluation.entity.SecondKpi;
+import net.tusdasa.evaluation.entity.ThirdKpi;
+
+import java.util.List;
+
 public interface StudentAssessmentService {
+    List<ThirdKpi> findAllThirdKpi(Integer secondKpiId);
+
+    List<SecondKpi> findAllSecondKpi(Integer firstKpiId);
 }
