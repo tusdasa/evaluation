1090bfd773d5e5fa060cc0d3ae7cbcaaefd072c2
tusdasa <none@none>
2020-02-02 15:28:01 +0800

    添加打包工具
    --HG--
    branch : develement

--- evaluation-service/student-assessment/src/main/java/net/tusdasa/evaluation/service/StudentAssessmentService.java
+++ evaluation-service/student-assessment/src/main/java/net/tusdasa/evaluation/service/StudentAssessmentService.java
@@ -1,12 +1,17 @@
 package net.tusdasa.evaluation.service;
 
+import net.tusdasa.evaluation.entity.FirstKpi;
 import net.tusdasa.evaluation.entity.SecondKpi;
 import net.tusdasa.evaluation.entity.ThirdKpi;
 
 import java.util.List;
 
 public interface StudentAssessmentService {
-    List<ThirdKpi> findAllThirdKpi(Integer secondKpiId);
+
+    List<FirstKpi> findAllFirstKpi(Integer roleId);
 
     List<SecondKpi> findAllSecondKpi(Integer firstKpiId);
+
+    List<ThirdKpi> findAllThirdKpi(Integer secondKpiId);
+
 }
---
+++ release.bat
@@ -0,0 +1,9 @@
+@echo off
+set Tss=%TIME:~6,2%
+set Tmm=%TIME:~3,2%
+set Thh=%TIME:~0,2%
+set Thh=%Thh: =0%
+set folder=%Thh%.%Tmm%.%Tss%
+md %BUILD_DIR%\bulid-%folder%
+copy %BUILD_DIR%\*.jar %BUILD_DIR%\bulid-%folder%\
+del /q %BUILD_DIR%\*.jar
