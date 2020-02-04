ea98b66253e7ca2e158671f9a0bb9f17a0842681
tusdasa <none@none>
2020-02-04 09:38:34 +0800

    修正发行脚本
    --HG--
    branch : develement

--- release.bat
+++ release.bat
@@ -3,7 +3,9 @@ set Tss=%TIME:~6,2%
 set Tmm=%TIME:~3,2%
 set Thh=%TIME:~0,2%
 set Thh=%Thh: =0%
-set folder=%Thh%.%Tmm%.%Tss%
-md %BUILD_DIR%\bulid-%folder%
-copy %BUILD_DIR%\*.jar %BUILD_DIR%\build-%folder%\
+set currentTime=%Thh%.%Tmm%.%Tss%
+set currentDay=%date:~,4%-%date:~5,2%-%date:~8,2%
+::md %BUILD_DIR%\build-%folder%
+md %BUILD_DIR%\build-%currentDay%-%currentTime%
+copy %BUILD_DIR%\*.jar %BUILD_DIR%\build-%currentDay%-%currentTime%\
 del /q %BUILD_DIR%\*.jar
