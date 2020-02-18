01142d5281151fab92efd8d12848b48a828864c8
tusdasa <none@none>
2020-02-18 15:16:35 +0800

    修复构建后的PHP脚本
    --HG--
    branch : develement

--- shell.php
+++ shell.php
@@ -1,14 +1,11 @@
 <?php
-
-    $email_to = "tusdasa@qq.com";
-    $email_title = "构建通知";
     $str = "";
     $shell = "hg log --limit 1";
     exec($shell, $result, $status);
 	for($a =0 ; $a < count($result) ;$a++){
-		$str = $str."<br />".$result[$a];
+		$str = $str."<br />".iconv('GB2312', 'UTF-8', $result[$a]);
 	}
-	$data = array("to" => $email_to, "title" => $email_title, "text" => $str);
+	$data = array("to" => "tusdasa@qq.com", "title" => "构建通知", "text" => $str);
 	$data_string = json_encode($data);
 	$ch = curl_init('http://127.0.0.1:8085/send2');
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
