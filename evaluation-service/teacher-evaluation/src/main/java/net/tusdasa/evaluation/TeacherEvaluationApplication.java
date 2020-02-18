f081a4917cd427cc95994a352314e06f44404596
tusdasa <none@none>
2020-02-18 20:39:40 +0800

    增加教师评测微服务
    --HG--
    branch : develement

---
+++ evaluation-service/teacher-evaluation/src/main/java/net/tusdasa/evaluation/TeacherEvaluationApplication.java
@@ -0,0 +1,11 @@
+package net.tusdasa.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class TeacherEvaluationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherEvaluationApplication.class, args);
+    }
+}
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,82 @@
+<template>
+  <el-container style="height: 100%; border: 1px solid #eee">
+  <el-aside style="background-color: rgb(238, 241, 246)">
+    <el-menu :default-openeds="['1']">
+      <el-image src="logo.png" fit="scale-down"></el-image>
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+    </el-menu>
+  </el-aside>
+  
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+    
+    <el-main>
+      <el-table :data="tableData">
+        <el-table-column prop="date" label="日期" width="140">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="地址">
+        </el-table-column>
+      </el-table>
+    </el-main>
+  </el-container>
+</el-container>
+</template>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+    height: 60px;
+  }
+  
+  .el-aside {
+    color: #333;
+    width: 200px;
+  }
+  .el-main{
+    height: 800px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(10).fill(item)
+      }
+    }
+  };
+</script>
