6da9eda5613395bef14bde7959c21b3c52feb658
tusdasa <none@none>
2020-02-20 11:59:26 +0800

    修正构建脚本
    --HG--
    branch : develement

--- shell.php
+++ shell.php
@@ -4,9 +4,9 @@
     exec($shell, $result, $status);
 	$str = "changeset: ".substr(iconv('GB2312', 'UTF-8', $result[0]),13);
 	$str = $str."<br />"."分支： ".substr(iconv('GB2312', 'UTF-8', $result[1]),13);
-	$str = $str."<br />"."提交者： ".substr(iconv('GB2312', 'UTF-8', $result[4]),12);
-	$str = $str."<br />"."提交时间： ".date("Y-m-d h:i:s a", strtotime(substr(iconv('GB2312', 'UTF-8', $result[5]),12)));
-	$str = $str."<br />"."提交信息: ".substr(iconv('GB2312', 'UTF-8', $result[6]),14);
+	$str = $str."<br />"."提交者： ".substr(iconv('GB2312', 'UTF-8', $result[4]),13);
+	$str = $str."<br />"."提交时间： ".date("Y-m-d h:i:s a", strtotime(substr(iconv('GB2312', 'UTF-8', $result[5]),13)));
+	$str = $str."<br />"."提交信息: ".substr(iconv('GB2312', 'UTF-8', $result[6]),13);
 
 	$email_text = array("to" => "tusdasa@qq.com", "title" => "构建通知", "text" => $str);
 	$email_data = json_encode($email_text);
@@ -26,7 +26,7 @@
     // send qq
     $qq_text = array("auto_escape" => true,
     "group_id" => 808154403,
-    "message" => "本次构建完成\n完成时间: ".date("Y-m-d h:i:s a")."\n详情: \n".substr(iconv('GB2312', 'UTF-8', $result[6]),14)
+    "message" => "本次构建完成\n完成时间: ".date("Y-m-d h:i:s a")."\n详情: \n".substr(iconv('GB2312', 'UTF-8', $result[6]),13)
     );
     print_r($qq_text);
 
